3cb065040a992dd9432137106110fd1e9fac20f3
soge0001 <gergely.soti@hs-karlsruhe.de>
2023-04-03 16:20:47 +0200

    servo node is launched at start

--- kuka_kr3_cell_description/launch/cell.launch.py
+++ kuka_kr3_cell_description/launch/cell.launch.py
@@ -3,6 +3,22 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 
 def generate_launch_description():
@@ -47,6 +63,37 @@ def generate_launch_description():
     eki_io_port = LaunchConfiguration("eki_io_port")
     n_io = LaunchConfiguration("n_io")
 
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("kuka_kr3_cell_description"), "urdf", "robot.urdf.xacro"]),
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "robot_ip:=",
+            robot_ip,
+            " ",
+            "eki_robot_port:=",
+            eki_robot_port,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("kuka_kr3_cell_description"), "config", "robot.srdf"]
+            ),
+        ]
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_content
+    }
+
     moveit_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution([FindPackageShare('kuka_common_moveit_config'), 'launch', 'demo.launch.py'])),
@@ -79,4 +126,21 @@ def generate_launch_description():
         }.items(),
     )
 
-    return LaunchDescription(declared_arguments + [moveit_launch, gripper_launch, moveit_wrapper_launch])
+    kinematics_yaml = load_yaml("kuka_common_moveit_config", "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+    servo_yaml = load_yaml("kuka_kr3_cell_description", "config/kuka_servo_config.yaml")
+    servo_params = {"moveit_servo": servo_yaml}
+
+    servo_node = Node(
+        package="moveit_wrapper",
+        executable="servo_server",
+        parameters=[
+            servo_params,
+            robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription(declared_arguments + [moveit_launch, gripper_launch, moveit_wrapper_launch, servo_node])
