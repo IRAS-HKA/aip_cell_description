82daa1fcad5917577d6a4985a2b6a1f78b3fc378
aip <mauricedroll@gmail.com>
2024-03-04 12:30:26 +0100

    Gripper restructured

--- aip_bosch_gripper/include/aip_bosch_gripper/gripper.h
+++ aip_bosch_gripper/include/aip_bosch_gripper/gripper.h
@@ -12,16 +12,27 @@ namespace aip_bosch_gripper
     public:
         Gripper();
         ~Gripper(){};
+        
+        //void open_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
+        //                  std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response);
+        //void close_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
+        //                   std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response);
+        // void executeCommands(int allPins[], bool valueOfPins[]);
+        //void controlCommands(int allPins[], bool valueOfPins[]);
+        
 
     private:
+        std::map<int, std::vector<int>> cylinder_pins;
+        void execute_commands(const std::vector<int>& pins, const std::vector<bool>& values);
+        void check_commands(const std::vector<int>& pins, const std::vector<bool>& values);
         std::string robot_ip_ = "robot_ip";
         std::shared_ptr<kuka_eki_io_interface::KukaEkiIOInterface> _kuka_eki_io_interface;
         rclcpp::Service<iras_interfaces::srv::MoveGripper>::SharedPtr _open_gripper_srv;
         rclcpp::Service<iras_interfaces::srv::MoveGripper>::SharedPtr _close_gripper_srv;
         void open_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
                           std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response);
-        void close_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
-                           std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response);
+        //void close_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
+        //                   std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response);
     };
 }
 #endif
--- aip_bosch_gripper/src/gripper.cpp
+++ aip_bosch_gripper/src/gripper.cpp
@@ -1,70 +1,28 @@
 #include <aip_bosch_gripper/gripper.h>
-//#include <iras_interfaces/OpenGripper.srv>
 
 using std::placeholders::_1;
 using std::placeholders::_2;
 
 namespace aip_bosch_gripper
-{    //
-    std::map<int, std::vector<int>> modeOfOperation;
-
+{       
     Gripper::Gripper() : Node("aip_bosch_gripper_node")
     {
-                /* Initalisierung der Modes
-        Mode 1: Alle Greifer ausfahren
-        Mode 2: Greifer 2 und 4 (große)
-        Mode 3: Greifer 1 und 3 (kleine)
-        Mode 4: Greifer 3 und 4 (links)
-        Mode 5: Greifer 1 und 2 (rechts)
-        Mode 6: Greifer 1 ausfahren
-        Mode 7: Greifer 2 ausfahren
-        Mode 8: Greifer 3 ausfahren
-        Mode 9: Greifer 4 ausfahren
+        // Mapping of the pins to the corresponding cylinders: 
+        // structure: extend_pin, retract_pin, extend_check_pin, retract_check_pin, suction_pin
+        cylinder_pins = {
+            {1, {40, 39, 132, 134, 33}},
+            {2, {42, 41, 136, 138, 34}},
+            {3, {44, 43, 133, 135, 35}},
+            {4, {46, 45, 137, 139, 36}}
+        };
 
-        Mode 10: Greifer 1 einfahren
-        Mode 11: Greifer 2 einfahren
-        Mode 12: Greifer 3 einfahren
-        Mode 13: Greifer 4 einfahren
-        */
-
-
-        // Put the values in the map 
-        // Insert only the single cylinders 
-        // build the other modes based on the single cylinders
-
-        modeOfOperation[6].push_back(40);
-        modeOfOperation[6].push_back(33);
-
-        modeOfOperation[7].push_back(42);
-        modeOfOperation[7].push_back(34);
-
-        /*
-        modeOfOperation[8].push_back(44);
-        modeOfOperation[8].push_back(35);
-
-        modeOfOperation[9].push_back(46);
-        modeOfOperation[9].push_back(36);
-
-        modeOfOperation[10].push_back(39);
-        modeOfOperation[10].push_back(33);
-
-        modeOfOperation[11].push_back(41);
-        modeOfOperation[11].push_back(34);
-
-        modeOfOperation[12].push_back(43);
-        modeOfOperation[12].push_back(35);
-
-        modeOfOperation[13].push_back(45);
-        modeOfOperation[13].push_back(36);
-        */
-
+        // Initalization of robot connection
         this->declare_parameter("robot_ip", "10.166.32.145");
         this->declare_parameter("eki_io_port", 54601);
         this->declare_parameter("n_io", 2);
-
         std::string robot_ip;
         this->get_parameter("robot_ip", robot_ip);
-        //this->get_parameter("robot_ip", "10.166.32.145");
+
         int eki_io_port;
         std::string eki_io_port_;
         this->get_parameter("eki_io_port", eki_io_port);
@@ -75,196 +33,185 @@ namespace aip_bosch_gripper
         _kuka_eki_io_interface.reset(new kuka_eki_io_interface::KukaEkiIOInterface(robot_ip.c_str(), eki_io_port_.c_str(), n_io));
 
         _open_gripper_srv = this->create_service<iras_interfaces::srv::MoveGripper>("open_gripper", std::bind(&Gripper::open_gripper, this, _1, _2));
-        _close_gripper_srv = this->create_service<iras_interfaces::srv::MoveGripper>("close_gripper", std::bind(&Gripper::close_gripper, this, _1, _2));
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "TEST");
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Ready to receive commands. TETTSTSTETTSTSTST");
+        //_close_gripper_srv = this->create_service<iras_interfaces::srv::MoveGripper>("close_gripper", std::bind(&Gripper::close_gripper, this, _1, _2));
+
+        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Ready to receive commands. ");
         RCLCPP_INFO(rclcpp::get_logger("Eigener Output: IP: "), robot_ip.c_str());
-
-
-        /*
-                // Zugriff auf die Werte
-                std::vector<int> mode12 = modeOfOperation["mode12"];
-
-                for(int value : mode12) {
-                    std::cout << value << std::endl;
-                }
-        */
-
-
     }
+
+    /* Method close_gripper
+
+    void Gripper::close_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
+                                std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "CLOSE GRIPPER erreicht");
+        
+        // std::string requestStr = std::to_string(request -> cylinder_ids);
+        // std::string responseStr = std::to_string(response -> success);
+                
+        // response->success = true;
+        // RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Close Gripper: Funktionsaufruf beendet!");
+        // RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "---------------*******-----------------");
+    }
+*/
 
     void Gripper::open_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
                                std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response)
     {
-        // Ausgabe der Werte
-        //std::string requestStr = std::to_string(request->data);
-        //std::string responseStr = std::to_string(response->success);
 
-        //  RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Response: %s", responseStr.c_str());
-        //  RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Request: %s", requestStr.c_str());
+        /* close_gripper contains the following movements per given cylinder: 
+                - eject cylinder 
+                - check if cylinder is ejected
+                - suction on (stays on => will be stopped in the open_gripper function)
+                - retract cylinder
+                - check if cylinder is retracted
+        */
         
         RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "OPEN GRIPPER erreicht");
 
-        rclcpp::Rate loop_rate(50);
-        // const std::vector<int> set_io_pins_cmd{8, 7};
 
+        // ######## needed later on ###########
+
+        // std::string requestStr = std::to_string(request -> cylinder_ids);
+        // std::string responseStr = std::to_string(response->success);
+        // RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Request: %s", requestStr.c_str());
+        // RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Response: %s", responseStr.c_str());
         
-        std::vector<int> set_io_pins_cmd;
-        set_io_pins_cmd.assign(modeOfOperation[7].begin(), modeOfOperation[7].end());
-
-        std::ostringstream oss;
-        for(int number : set_io_pins_cmd) {
-            oss << number << ' ';
-        }
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "set_io_pins_cmd: %s", oss.str().c_str());
-
-        //RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"),modeOfOperation["mode6"].begin());
-        // RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"),modeOfOperation["mode6"].end());
-
-        //const std::vector<int> set_io_pins_cmd{36, 46};
-        const std::vector<int> set_io_modes_cmd{
-            2,
-            2,
-        };
-        //const std::vector<bool> set_target_ios_cmd{false, true};
-        const std::vector<bool> set_target_ios_cmd{true, false};
-
-        bool f_pin_set = false;
-        bool s_pin_set = false;
-        bool f_command_received = false;
-        bool s_command_received = false;
-        bool f_mode_set = false;
-        bool s_mode_set = false;
-        bool command_received = false;
-        std::vector<bool> io_states;
-        std::vector<int> io_pins;
-        std::vector<int> io_types;
-        int buff_len;
-        // TODO: add timeout und mache Abbruchbedingung rein
-        while (!command_received && rclcpp::ok())
+        std::vector<int> eject_pins;
+        std::vector<int> retract_pins;
+        std::vector<int> eject_check_pins;
+        std::vector<int> retract_check_pins;
+        std::vector<int> suction_pins;
+        for (auto const& cylinder_id : request->cylinder_ids)
         {
-            _kuka_eki_io_interface->eki_write_command(set_io_pins_cmd, set_io_modes_cmd, set_target_ios_cmd);
-            loop_rate.sleep();
-            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
-            f_pin_set = io_pins[0] == set_io_pins_cmd[0];
-            s_pin_set = io_pins[1] == set_io_pins_cmd[1];
-            f_command_received = io_states[0] == set_target_ios_cmd[0];
-            s_command_received = io_states[1] == set_target_ios_cmd[1];
-            f_mode_set = io_types[0] == set_io_modes_cmd[0];
-            s_mode_set = io_types[1] == set_io_modes_cmd[1];
-            command_received = f_pin_set && s_pin_set && f_command_received && s_command_received && f_mode_set && s_mode_set;
-            //const std::vector<bool> set_target_ios_cmd{false, true};
-            RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Ende erste Schleife, Open Gripper erreicht");
-
-
+            eject_pins.push_back(cylinder_pins[cylinder_id][0]);
+            retract_pins.push_back(cylinder_pins[cylinder_id][1]);
+            eject_check_pins.push_back(cylinder_pins[cylinder_id][2]);
+            retract_check_pins.push_back(cylinder_pins[cylinder_id][3]);
+            suction_pins.push_back(cylinder_pins[cylinder_id][4]);
         }
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Open Gripper: Erste Schleife beendet!");
-
-        bool opened = false;
-        // const std::vector<int> io_pins_cmd{7, 8};
-        const std::vector<int> io_pins_cmd{42, 34};
-        const std::vector<int> io_modes_cmd{1, 1};
-        const std::vector<bool> target_ios_cmd{true, false};
-        // TODO: add timeout
-        while (!opened && rclcpp::ok())
+        
+        //pins {c1_aus, c2_aus, c3_aus}
+        if (eject_pins.size() % 2 != 0)
         {
-            _kuka_eki_io_interface->eki_write_command(io_pins_cmd, io_modes_cmd, target_ios_cmd);
-            loop_rate.sleep();
-            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
-            f_pin_set = io_pins[0] == io_pins_cmd[0];
-            RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Wert für Pin %d : %d", io_pins_cmd[0], io_pins[0]);
-            s_pin_set = io_pins[1] == io_pins_cmd[1];
-            f_command_received = io_states[0] == true;
-            s_command_received = io_states[1] == false;
-            f_mode_set = io_types[0] == io_modes_cmd[0];
-            s_mode_set = io_types[1] == io_modes_cmd[1];
-            opened = f_pin_set && s_pin_set && f_command_received && s_command_received && f_mode_set && s_mode_set;
+            eject_pins.push_back(eject_pins[0]);
+            retract_pins.push_back(retract_pins[0]);
+            eject_check_pins.push_back(eject_check_pins[0]);
+            retract_check_pins.push_back(retract_check_pins[0]);
+            suction_pins.push_back(suction_pins[0]);
         }
+
+        //pins {c1_aus, c2_aus, c3_aus, c1_aus}
+        //for (std::vector<int>::size_type i = 0; i < eject_pins.size(); i+=2)
+        for (int i = 0; i < eject_pins.size(); i+=2)
+        {
+            // eject cylinder
+            execute_commands({eject_pins[i], eject_pins[i+1]}, {true, true});
+        }
+        for (int i = 0; i < eject_pins.size(); i+=2)
+        {
+            // check if cylinder is ejected
+            check_commands({eject_check_pins[i], eject_check_pins[i+1]}, {true, true});
+            // reset the ejection pin command
+            execute_commands({eject_pins[i], eject_pins[i+1]}, {false, false});  
+        }
+
+        for (int i = 0; i < suction_pins.size(); i+=2)
+        {
+            // suction on 
+            execute_commands({suction_pins[i], suction_pins[i+1]}, {true, true});   // change to false because open_gripper is not responsible for suction
+        }
+        for (int i = 0; i < retract_pins.size(); i+=2)
+        {
+            // retract cylinder
+            execute_commands({retract_pins[i], retract_pins[i+1]}, {true, true});
+        }
+        for (int i = 0; i < retract_pins.size(); i+=2)
+        {
+            // check if cylinder is retracted
+            check_commands({retract_check_pins[i], retract_check_pins[i+1]}, {true, true});
+            // reset the retraction pin command
+            execute_commands({retract_pins[i], retract_pins[i+1]}, {false, false});
+        }
+
         response->success = true;
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Open Gripper: Funktionsaufruf beendet!");
+        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "OPEN GRIPPER: Funktionsaufruf beendet!");
         RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "---------------*******-----------------");
 
     }
 
-    void Gripper::close_gripper(const std::shared_ptr<iras_interfaces::srv::MoveGripper::Request> request,
-                                std::shared_ptr<iras_interfaces::srv::MoveGripper::Response> response)
-    //void Gripper::close_gripper(std::shared_ptr<OpenGripperSrv::Request> request,
-    //                            std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+    void Gripper::execute_commands(const std::vector<int>& pins, const std::vector<bool>& values)
     {
-        // Ausgabe der Werte
-        //std::string requestStr = std::to_string(request -> message );
-        //std::string responseStr = std::to_string(response);
-
-        //RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Request: %s", requestStr);
-        //RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Response: %s", responseStr.c_str());
-
-
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "CLOSE GRIPPER erreicht");
+    
         rclcpp::Rate loop_rate(50);
-        // const std::vector<int> set_io_pins_cmd{36, 45};
-        std::vector<int> set_io_pins_cmd;
-        set_io_pins_cmd.assign(modeOfOperation[11].begin(), modeOfOperation[11].end());
 
-        std::ostringstream oss;
-        for(int number : set_io_pins_cmd) {
-            oss << number << ' ';
-        }
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "set_io_pins_cmd: %s", oss.str().c_str());
+        // static variables for eki_write_command (SetOutput -> see EkiIOInterface.xml)
+        const std::vector<int> set_io_types{2, 2};
 
+        // variables for eki_read_state
+        int buff_len;
+        std::vector<bool> io_states;
+        std::vector<int> io_pins;
+        std::vector<int> io_types;
 
-
-        const std::vector<int> set_io_modes_cmd{2, 2};
-        const std::vector<bool> set_target_ios_cmd{true, false};
-
-        bool f_pin_set = false;
-        bool s_pin_set = false;
+        // declaration of comparison valiables
         bool f_command_received = false;
         bool s_command_received = false;
         bool f_mode_set = false;
         bool s_mode_set = false;
+
         bool command_received = false;
+
+        while (!command_received)
+        {
+            // _kuka_eki_io_interface->eki_write_command(set_io_pins_cmd, set_io_modes_cmd, set_target_ios_cmd);
+            _kuka_eki_io_interface->eki_write_command(pins, set_io_types, values);
+
+            loop_rate.sleep();
+            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
+            f_command_received = io_states[0] == values[0];
+            s_command_received = io_states[1] == values[1];
+            f_mode_set = io_types[0] == pins[0];
+            s_mode_set = io_types[1] == pins[1];
+           
+            command_received = f_command_received && s_command_received && f_mode_set && s_mode_set;
+        }
+    }
+
+    void Gripper::check_commands(const std::vector<int>& pins, const std::vector<bool>& values)
+    {
+      
+        rclcpp::Rate loop_rate(50);
+
+        // static variables for eki_write_command (GetInput -> see EkiIOInterface.xml)
+        const std::vector<int> set_io_types{1, 1};
+        
+        // variables for eki_read_state
         std::vector<bool> io_states;
         std::vector<int> io_pins;
         std::vector<int> io_types;
         int buff_len;
-        // TODO: add timeout
-        while (!command_received && rclcpp::ok())
-        {
-            _kuka_eki_io_interface->eki_write_command(set_io_pins_cmd, set_io_modes_cmd, set_target_ios_cmd);
-            loop_rate.sleep();
-            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
-            f_pin_set = io_pins[0] == set_io_pins_cmd[0];
-            s_pin_set = io_pins[1] == set_io_pins_cmd[1];
-            f_command_received = io_states[0] == set_target_ios_cmd[0];
-            s_command_received = io_states[1] == set_target_ios_cmd[1];
-            f_mode_set = io_types[0] == set_io_modes_cmd[0];
-            s_mode_set = io_types[1] == set_io_modes_cmd[1];
-            command_received = f_pin_set && s_pin_set && f_command_received && s_command_received && f_mode_set && s_mode_set;
-        }
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Ende erreicht: Greifer sollte geschlossen sein.");
-        bool closed = false;
-        const std::vector<int> io_pins_cmd{36, 46};
-        const std::vector<int> io_modes_cmd{1, 1};
-        const std::vector<bool> target_ios_cmd{false, false};
-        // TODO: add timeout
-        while (!closed && rclcpp::ok())
-        {
-             RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Beginn 2te Schleife.");
-            _kuka_eki_io_interface->eki_write_command(io_pins_cmd, io_modes_cmd, target_ios_cmd);
-            loop_rate.sleep();
-            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
-            f_pin_set = io_pins[0] == io_pins_cmd[0];
-            s_pin_set = io_pins[1] == io_pins_cmd[1];
-            f_command_received = io_states[0] == false;
-            s_command_received = io_states[1] == true;
-            f_mode_set = io_types[0] == io_modes_cmd[0];
-            s_mode_set = io_types[1] == io_modes_cmd[1];
-            closed = f_pin_set && s_pin_set && f_command_received && s_command_received && f_mode_set && s_mode_set;
-            RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Ende 2 Schleife erreicht: Greifer sollte geschlossen sein.");
-        }
-        response->success = true;
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "Close Gripper: Funktionsaufruf beendet!");
-        RCLCPP_INFO(rclcpp::get_logger("aip_bosch_gripper_node"), "---------------*******-----------------");
 
+        // declaration of comparison valiables
+        bool f_command_received = false;
+        bool s_command_received = false;
+        bool f_mode_set = false;
+        bool s_mode_set = false;
+
+        bool opened = false;
+
+        while (!opened)
+        {
+            _kuka_eki_io_interface->eki_write_command(pins, set_io_types, values);
+            loop_rate.sleep();
+            _kuka_eki_io_interface->eki_read_state(io_states, io_pins, io_types, buff_len);
+            f_command_received = io_states[0] == true;
+            s_command_received = io_states[1] == false;
+            f_mode_set = io_types[0] == set_io_types[0];
+            s_mode_set = io_types[1] == set_io_types[1];
+            opened = f_command_received && s_command_received && f_mode_set && s_mode_set;
+        
+        }
+        
     }
 }
