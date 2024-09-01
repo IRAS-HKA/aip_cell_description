0bca5846ab18ed00508fd64fb5a34c96600d68d2
Eshan Savla <eshan.savla@gmail.com>
2024-09-01 18:29:13 +0530

    fix hardware flag

--- aip_cell_description/launch/aip.launch.py
+++ aip_cell_description/launch/aip.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
     ))
     declared_arguments.append(DeclareLaunchArgument(
         "use_fake_hardware",
-        default_value="True",
+        default_value="true",
         description="Start robot with fake hardware mirroring command to its states.",
     ))
     declared_arguments.append(DeclareLaunchArgument(
