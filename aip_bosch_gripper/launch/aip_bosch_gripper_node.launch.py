411781caa5c9e0a76b6dc8890b0751de4fb7304c
aip_w04 <aip@aip.com>
2024-12-16 15:07:30 +0100

    Simplify launch file

--- aip_bosch_gripper/launch/aip_bosch_gripper_node.launch.py
+++ aip_bosch_gripper/launch/aip_bosch_gripper_node.launch.py
@@ -2,39 +2,36 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 
 
 def generate_launch_description():
     declared_arguments = []
-
     declared_arguments.append(DeclareLaunchArgument(
         "robot_ip",
         default_value="10.166.32.145",
         description="IP address by which the robot can be reached."
     ))
-
     declared_arguments.append(DeclareLaunchArgument(
         "eki_io_port",
         default_value="54601",
         description="Port by which the robot can be reached."
     ))
-
     declared_arguments.append(DeclareLaunchArgument(
         "n_io",
         default_value="2",
         description="Port by which the robot can be reached."
     ))
-
     declared_arguments.append(DeclareLaunchArgument(
-        "only_visualize",
-        default_value="false",
-        description="If true, the node will only visualize the gripper state."
+        "use_fake_hardware",
+        default_value="True",
+        description="Start robot with fake hardware mirroring command to its states.",
     ))
 
     robot_ip = LaunchConfiguration("robot_ip")
     eki_io_port = LaunchConfiguration("eki_io_port")
     n_io = LaunchConfiguration("n_io")
-    only_visualize = LaunchConfiguration("only_visualize")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
 
     eki_io_node = Node(
         package="aip_bosch_gripper",
@@ -44,27 +41,15 @@ def generate_launch_description():
              "eki_io_port": eki_io_port,
              "n_io": n_io}
         ],
-        output='screen'
+        output='screen',
+        condition=UnlessCondition(use_fake_hardware)
     )
-    print(f"Flag val: {only_visualize}")
-    if not only_visualize == "true":
-        print("Starting gripper node with IO")
-        return LaunchDescription(declared_arguments + [
-            eki_io_node, 
-            Node(
-                package="aip_bosch_gripper",
-                executable="GripperVisualizerNode.py",
-                name="gripper_visualizer_node",
-                output='screen',
-            )
-        ])
-    else:
-        print("Starting gripper node without IO")
-        return LaunchDescription(declared_arguments + [
-            Node(
-                package="aip_bosch_gripper",
-                executable="GripperVisualizerNode.py",
-                name="gripper_visualizer_node",
-                output='screen',
-            )
-        ])
+    only_visualizer = Node(
+        package="aip_bosch_gripper",
+        executable="GripperVisualizerNode.py",
+        name="gripper_visualizer_node",
+        output='screen',
+    )
+    
+    return LaunchDescription(declared_arguments + [eki_io_node, only_visualizer])
+    
